8c6fbd6c07eb6bea924b2fa7ece6e7b72a54b954
crepes <camilo.gonzalez@crepesywaffles.com>
2021-08-03 09:46:50 -0500

    añadir funcion personalizada multipart

--- PortalBaack/api/facturacion/controllers/facturacion.js
+++ PortalBaack/api/facturacion/controllers/facturacion.js
@@ -4,5 +4,16 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
-
-module.exports = {};
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+module.exports = {
+    async create(ctx) {
+        let entity;
+        if (ctx.is('multipart')) {
+          const { data, files } = parseMultipartData(ctx);
+          entity = await strapi.services.facturacion.create(data, { files });
+        } else {
+          entity = await strapi.services.facturacion.create(ctx.request.body);
+        }
+        return sanitizeEntity(entity, { model: strapi.models.facturacion });
+      },
+};
